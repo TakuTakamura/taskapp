7cfc89745d48be733bd017faa4ddb00056812fbc
taku.takamura <taku.takamura@tecahademy.jp>
2019-10-23 09:00:54 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 高村拓 on 2019/10/23.
+//  Copyright © 2019 taku.takamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
